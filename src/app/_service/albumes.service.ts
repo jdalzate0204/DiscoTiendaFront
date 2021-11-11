9d09a871b68d6fc73b6a60e60625300cf740361a
jdalzate0204 <james.alzate22@hotmail.com>
2021-11-11 11:17:59 -0500

    Servicio crear artista

--- src/app/_service/albumes.service.ts
+++ src/app/_service/albumes.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Album } from '../_model/Album';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumService {
 
+  private URL: string = environment.API + '/albumes';
+
   constructor(private http: HttpClient) { }
+
+  postCrearAlbum(album: Album){
+    return this.http.post<Album>(this.URL + "/guardar", album);
+  }
 }
