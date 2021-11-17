8da237c6ab5cb5d886235be8539f9dc968e0cbd7
jdalzate0204 <james.alzate22@hotmail.com>
2021-11-17 12:40:14 -0500

    Avance catalogo

--- src/app/components/Tienda/catalogo/catalogo.component.ts
+++ src/app/components/Tienda/catalogo/catalogo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Album } from 'src/app/_model/Album';
+import { AlbumService } from 'src/app/_service/albumes.service';
 
 @Component({
   selector: 'app-catalogo',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogoComponent implements OnInit {
 
-  constructor() { }
+  albumes!: Album[];
+  catalogo!: Album[]; 
+  formatoMoneda!: Intl.NumberFormat;
+  dataSource!: MatTableDataSource<Album>;
+
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit(): void {
+    this.albumService.getListarAlbumes().subscribe( data => {
+      this.albumes = data;
+      this.catalogo = data;
+    });
   }
 
 }
